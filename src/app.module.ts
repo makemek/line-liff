933520d41da9f2fb4fa7becfa52e3f98860396b8
Apipol Niyomsak <apipol.niyomsak@kingpower.com>
2019-12-07 09:51:25 +0700

    remove unuse file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { sharedModules } from './shared'
 import { BackofficeModule } from './backoffice'
 
 @Module({
   imports: [...sharedModules, BackofficeModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
