dc9bb7abf64615cce3392e94347b0e39706dc596
Apipol Niyomsak <apipol.niyomsak@kingpower.com>
2019-12-08 01:42:23 +0700

    can add product

--- public/cms.js
+++ public/cms.js
@@ -1,3 +1,19 @@
-const app = new Vue({
+new Vue({
   el: '#app',
+  data() {
+    return {
+      newProduct: {
+        name: undefined,
+        image: undefined,
+        price: undefined,
+      },
+    }
+  },
+  methods: {
+    async onAddProduct() {
+      const { name, image, price } = this.newProduct
+      await axios.post('/backoffice/product', { name, image, price })
+      window.location.reload()
+    },
+  },
 })
