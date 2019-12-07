dd49f7f3e329714320c03ec5387d8a107339674a
Apipol Niyomsak <apipol.niyomsak@kingpower.com>
2019-12-08 02:20:27 +0700

    fetch and display

--- public/cms.js
+++ public/cms.js
@@ -7,13 +7,24 @@ new Vue({
         image: undefined,
         price: undefined,
       },
+      fetching: false,
+      products: [],
     }
+  },
+  created() {
+    this.getProducts()
   },
   methods: {
     async onAddProduct() {
       const { name, image, price } = this.newProduct
       await axios.post('/backoffice/product', { name, image, price })
       window.location.reload()
+    },
+    async getProducts() {
+      this.fetching = true
+      const { data } = await axios.get('/backoffice/product')
+      this.products = data.products
+      this.fetching = false
     },
   },
 })
