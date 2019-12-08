3e38aa5c49c5bed1bfe5d3132eeaa48e2b9318a2
Apipol Niyomsak <apipol.niyomsak@kingpower.com>
2019-12-09 01:06:24 +0700

    display products

---
+++ public/customer.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      productLoading: false,
+      products: [],
+    }
+  },
+  created() {
+    this.getProducts()
+  },
+  methods: {
+    async getProducts() {
+      this.productLoading = true
+      const { data } = await axios.get('/product')
+      this.products = data.products
+      this.productLoading = false
+    },
+  },
+})
